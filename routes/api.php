b4384762c675cc807c97aec3e93c477e29dad4a3
Server <server@gmail.com>
2022-07-18 09:52:10 +0000

    feat: 重新調整 routes api

--- routes/api.php
+++ routes/api.php
@@ -32,6 +32,8 @@ Route::group(['prefix' => 'auth'], function () {
 });
 
 Route::middleware(['auth:api', LanguageMiddleware::class])->group(function () {
+    /** @var 基本資料操作 */
+
     // 部門管理
     Route::apiResource('/departments', Controllers\DepartmentController::class);
 
@@ -40,12 +42,32 @@ Route::middleware(['auth:api', LanguageMiddleware::class])->group(function () {
 
     // 幣值管理
     Route::apiResource('/currencies', Controllers\CurrencyController::class);
+
+    // 專案管理
+    Route::apiResource('/projects', Controllers\ProjectController::class);
+
+    // 銀行管理
+    Route::apiResource('/banks', Controllers\BankController::class);
+
+    // 人員管理
+    Route::apiResource('/staffs', Controllers\StaffController::class);
+
+    /** @var 產品操作 */
+
+    // 產品分類
+    Route::apiResource('/categories', Controllers\CategoryController::class);
+
+    // 產品管理
+    Route::apiResource('/products', Controllers\ProductController::class);
+
+    // 倉庫管理
+    Route::apiResource('/storehouses', Controllers\StorehouseController::class);
 });
 
 // 測試用
 Route::middleware(LanguageMiddleware::class)->group(function () {
     // 部門管理
-    Route::apiResource('/departments', Controllers\DepartmentController::class);
-    Route::apiResource('/jobs', Controllers\JobController::class);
+    // Route::apiResource('/departments', Controllers\DepartmentController::class);
+    // Route::apiResource('/jobs', Controllers\JobController::class);
     // Route::apiResource('/currencies', Controllers\CurrencyController::class);
 });
