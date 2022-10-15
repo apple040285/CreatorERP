bb96807ac2e41bba2907788b153e8a20c3eff566
Ryan <apple040285.1@gmail.com>
2022-10-15 12:05:07 +0800

    feat: 編譯前端檔案

---
+++ public/js/chunk-2d2258b2.4f7f6b83.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d2258b2"],{e59e:function(e,n,t){"use strict";t.r(n);var u=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("h5",[e._v("27")])},c=[],l=t("2877"),r={},s=Object(l["a"])(r,u,c,!1,null,null,null);n["default"]=s.exports}}]);
+//# sourceMappingURL=chunk-2d2258b2.4f7f6b83.js.map
