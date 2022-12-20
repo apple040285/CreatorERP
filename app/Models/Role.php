954b4e018c49e8e5bad6a20c685a962d06d8b86b
Server <server@gmail.com>
2022-12-20 17:25:26 +0800

    feat: 更新角色搜尋氣

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -4,6 +4,9 @@ namespace App\Models;
 
 class Role extends \Spatie\Permission\Models\Role
 {
+    // 搜尋器
+    use \Laravel\Scout\Searchable;
+
     protected $casts = [
         'created_at'        => 'datetime:Y-m-d',
         'updated_at'        => 'datetime:Y-m-d',
