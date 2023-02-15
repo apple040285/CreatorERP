de18a1b5b189f1babea275558bd3342242b68c77
Ryan <apple040285.1@gmail.com>
2023-02-15 18:57:56 +0800

    feat: 更新前端進貨憑單

--- frontend/src/router/routes/purchaseOperation.js
+++ frontend/src/router/routes/purchaseOperation.js
@@ -1,122 +1,108 @@
 export default [
-    {
-        path: '/PurchaseOperation/PurchaseVoucherList',
-        name: 'PurchaseOperation-PurchaseVoucherList',
-        component: () => import('@/views/purchase-operation/purchase-voucher/List.vue'),
-        meta: {
-            pageTitle: 'Purchase Voucher',
-            breadcrumb: [
-                { text: 'Purchase Operation' },
-                { text: 'Purchase Voucher', active: true },
-            ],
-        },
+  {
+    path: '/PurchaseOperation/PurchaseVoucherList',
+    name: 'PurchaseOperation-PurchaseVoucherList',
+    component: () => import('@/views/purchase-operation/purchase-voucher/PurchaseVoucherList.vue'),
+    meta: {
+      pageTitle: 'Purchase Voucher',
+      breadcrumb: [
+        { text: 'Purchase Operation' },
+        { text: 'Purchase Voucher', active: true },
+      ],
     },
-    {
-        path: '/PurchaseOperation/PurchaseVoucherCreate',
-        name: 'PurchaseOperation-PurchaseVoucherCreate',
-        component: () => import('@/views/purchase-operation/purchase-voucher/CrudEdit.vue'),
-        meta: {
-            pageTitle: 'Purchase Voucher',
-            navActiveLink: 'PurchaseOperation-PurchaseVoucherList',
-            breadcrumb: [
-                { text: 'Purchase Operation' },
-                { text: 'Purchase Voucher', to: { name: 'PurchaseOperation-PurchaseVoucherList' } },
-                { text: 'create', active: true },
-            ],
-        },
+  },
+  {
+    path: '/PurchaseOperation/PurchaseVoucherCreate',
+    name: 'PurchaseOperation-PurchaseVoucherCreate',
+    component: () => import('@/views/purchase-operation/purchase-voucher/PurchaseVoucherEdit.vue'),
+    meta: {
+      pageTitle: 'Purchase Voucher',
+      navActiveLink: 'PurchaseOperation-PurchaseVoucherList',
+      breadcrumb: [
+        { text: 'Purchase Operation' },
+        { text: 'Purchase Voucher', to: { name: 'PurchaseOperation-PurchaseVoucherList' } },
+        { text: 'create', active: true },
+      ],
     },
-    {
-        path: '/PurchaseOperation/PurchaseVoucherEdit',
-        name: 'PurchaseOperation-PurchaseVoucherEdit',
-        component: () => import('@/views/purchase-operation/purchase-voucher/CrudEdit.vue'),
-        meta: {
-            pageTitle: 'Purchase Voucher',
-            navActiveLink: 'PurchaseOperation-PurchaseVoucherList',
-            breadcrumb: [
-                { text: 'Purchase Operation' },
-                { text: 'Purchase Voucher', to: { name: 'PurchaseOperation-PurchaseVoucherList' } },
-                { text: 'edit', active: true },
-            ],
-        },
+  },
+  {
+    path: '/PurchaseOperation/PurchaseVoucherEdit/:id',
+    name: 'PurchaseOperation-PurchaseVoucherEdit',
+    component: () => import('@/views/purchase-operation/purchase-voucher/PurchaseVoucherEdit.vue'),
+    meta: {
+      pageTitle: 'Purchase Voucher',
+      navActiveLink: 'PurchaseOperation-PurchaseVoucherList',
+      breadcrumb: [
+        { text: 'Purchase Operation' },
+        { text: 'Purchase Voucher', to: { name: 'PurchaseOperation-PurchaseVoucherList' } },
+        { text: 'edit', active: true },
+      ],
     },
-    {
-        path: '/PurchaseOperation/PurchaseVoucherDetail',
-        name: 'PurchaseOperation-PurchaseVoucherDetail',
-        component: () => import('@/views/purchase-operation/purchase-voucher/Detail.vue'),
-        meta: {
-            pageTitle: 'Purchase Voucher',
-            navActiveLink: 'PurchaseOperation-PurchaseVoucherList',
-            breadcrumb: [
-                { text: 'Purchase Operation' },
-                { text: 'Purchase Voucher', to: { name: 'PurchaseOperation-PurchaseVoucherList' } },
-                { text: 'detail', active: true },
-            ],
-        },
+  },
+  {
+    path: '/PurchaseOperation/ReturnedPurchaseInvoiceList',
+    name: 'PurchaseOperation-ReturnedPurchaseInvoiceList',
+    component: () => import('@/views/purchase-operation/returned-purchase-invoice/List.vue'),
+    meta: {
+      pageTitle: 'Returned Purchase Invoice',
+      breadcrumb: [
+        { text: 'Purchase Operation' },
+        { text: 'Returned Purchase Invoice', active: true },
+      ],
     },
-    {
-        path: '/PurchaseOperation/ReturnedPurchaseInvoiceList',
-        name: 'PurchaseOperation-ReturnedPurchaseInvoiceList',
-        component: () => import('@/views/purchase-operation/returned-purchase-invoice/List.vue'),
-        meta: {
-            pageTitle: 'Returned Purchase Invoice',
-            breadcrumb: [
-                { text: 'Purchase Operation' },
-                { text: 'Returned Purchase Invoice', active: true },
-            ],
-        },
+  },
+  {
+    path: '/PurchaseOperation/ReturnedPurchaseInvoiceCreate',
+    name: 'PurchaseOperation-ReturnedPurchaseInvoiceCreate',
+    component: () => import('@/views/purchase-operation/returned-purchase-invoice/Crud.vue'),
+    meta: {
+      pageTitle: 'Returned Purchase Invoice',
+      navActiveLink: 'PurchaseOperation-ReturnedPurchaseInvoiceList',
+      breadcrumb: [
+        { text: 'Purchase Operation' },
+        { text: 'Returned Purchase Invoice', to: { name: 'PurchaseOperation-ReturnedPurchaseInvoiceList' } },
+        { text: 'create', active: true },
+      ],
     },
-    {
-        path: '/PurchaseOperation/ReturnedPurchaseInvoiceCreate',
-        name: 'PurchaseOperation-ReturnedPurchaseInvoiceCreate',
-        component: () => import('@/views/purchase-operation/returned-purchase-invoice/Crud.vue'),
-        meta: {
-            pageTitle: 'Returned Purchase Invoice',
-            navActiveLink: 'PurchaseOperation-ReturnedPurchaseInvoiceList',
-            breadcrumb: [
-                { text: 'Purchase Operation' },
-                { text: 'Returned Purchase Invoice', to: { name: 'PurchaseOperation-ReturnedPurchaseInvoiceList' } },
-                { text: 'create', active: true },
-            ],
-        },
+  },
+  {
+    path: '/PurchaseOperation/ReturnedPurchaseInvoiceEdit',
+    name: 'PurchaseOperation-ReturnedPurchaseInvoiceEdit',
+    component: () => import('@/views/purchase-operation/returned-purchase-invoice/Crud.vue'),
+    meta: {
+      pageTitle: 'Returned Purchase Invoice',
+      navActiveLink: 'PurchaseOperation-ReturnedPurchaseInvoiceList',
+      breadcrumb: [
+        { text: 'Purchase Operation' },
+        { text: 'Returned Purchase Invoice', to: { name: 'PurchaseOperation-ReturnedPurchaseInvoiceList' } },
+        { text: 'edit', active: true },
+      ],
     },
-    {
-        path: '/PurchaseOperation/ReturnedPurchaseInvoiceEdit',
-        name: 'PurchaseOperation-ReturnedPurchaseInvoiceEdit',
-        component: () => import('@/views/purchase-operation/returned-purchase-invoice/Crud.vue'),
-        meta: {
-            pageTitle: 'Returned Purchase Invoice',
-            navActiveLink: 'PurchaseOperation-ReturnedPurchaseInvoiceList',
-            breadcrumb: [
-                { text: 'Purchase Operation' },
-                { text: 'Returned Purchase Invoice', to: { name: 'PurchaseOperation-ReturnedPurchaseInvoiceList' } },
-                { text: 'edit', active: true },
-            ],
-        },
+  },
+  {
+    path: '/PurchaseOperation/ReturnedPurchaseInvoiceDetail',
+    name: 'PurchaseOperation-ReturnedPurchaseInvoiceDetail',
+    component: () => import('@/views/purchase-operation/returned-purchase-invoice/Detail.vue'),
+    meta: {
+      pageTitle: 'Returned Purchase Invoice',
+      navActiveLink: 'PurchaseOperation-ReturnedPurchaseInvoiceList',
+      breadcrumb: [
+        { text: 'Purchase Operation' },
+        { text: 'Returned Purchase Invoice', to: { name: 'PurchaseOperation-ReturnedPurchaseInvoiceList' } },
+        { text: 'detail', active: true },
+      ],
     },
-    {
-        path: '/PurchaseOperation/ReturnedPurchaseInvoiceDetail',
-        name: 'PurchaseOperation-ReturnedPurchaseInvoiceDetail',
-        component: () => import('@/views/purchase-operation/returned-purchase-invoice/Detail.vue'),
-        meta: {
-            pageTitle: 'Returned Purchase Invoice',
-            navActiveLink: 'PurchaseOperation-ReturnedPurchaseInvoiceList',
-            breadcrumb: [
-                { text: 'Purchase Operation' },
-                { text: 'Returned Purchase Invoice', to: { name: 'PurchaseOperation-ReturnedPurchaseInvoiceList' } },
-                { text: 'detail', active: true },
-            ],
-        },
+  },
+  {
+    path: '/PurchaseOperation/PurchaseDiscountVoucher',
+    name: 'PurchaseOperation-PurchaseDiscountVoucher',
+    component: () => import('@/views/purchase-operation/PurchaseDiscountVoucher.vue'),
+    meta: {
+      pageTitle: 'Purchase Discount Voucher',
+      breadcrumb: [
+        { text: 'Purchase Operation' },
+        { text: 'Purchase Discount Voucher', active: true },
+      ],
     },
-    {
-        path: '/PurchaseOperation/PurchaseDiscountVoucher',
-        name: 'PurchaseOperation-PurchaseDiscountVoucher',
-        component: () => import('@/views/purchase-operation/PurchaseDiscountVoucher.vue'),
-        meta: {
-            pageTitle: 'Purchase Discount Voucher',
-            breadcrumb: [
-                { text: 'Purchase Operation' },
-                { text: 'Purchase Discount Voucher', active: true },
-            ],
-        },
-    },
+  },
 ]
---
+++ public/js/chunk-2d0de124.bd72fa81.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0de124"],{"83a4":function(n,e,t){"use strict";t.r(e);var u=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("h5",[n._v("30")])},c=[],l=t("2877"),a={},r=Object(l["a"])(a,u,c,!1,null,null,null);e["default"]=r.exports}}]);
+//# sourceMappingURL=chunk-2d0de124.bd72fa81.js.map
