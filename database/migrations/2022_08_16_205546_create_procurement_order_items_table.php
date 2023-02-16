f3e9641f0dd0f3d6539cfc93b98796aaabc14760
張銘軒 <z309312002@gmail.com>
2023-02-16 14:11:36 +0800

    fix:修正欄位

--- database/migrations/2022_08_16_205546_create_procurement_order_items_table.php
+++ database/migrations/2022_08_16_205546_create_procurement_order_items_table.php
@@ -20,9 +20,6 @@ return new class extends Migration
             $table->unsignedBigInteger('storehouse_id')->comment('關聯所屬倉庫');
             $table->integer('quantity')->comment('數量');
             $table->float('price')->comment('單價');
-            $table->float('discount_amount')->nullable()->comment('折扣');
-            $table->float('tax_excluding_amount')->comment('未稅金額');
-            $table->float('tax_amount')->comment('稅金');
             $table->float('amount')->comment('金額');
             $table->timestamp('delivery_date')->useCurrent()->comment('預交日期');
             $table->string('remark')->nullable()->comment('備註');
